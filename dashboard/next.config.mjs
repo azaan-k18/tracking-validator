3bdb432615f64d6049294e81ac59dd85c16a7ef0
azaan-ie <azaan.khan@indianexpress.com>
2026-03-06 15:45:45 +0530

    Fixed api endpoint

--- dashboard/next.config.mjs
+++ dashboard/next.config.mjs
@@ -8,7 +8,7 @@ const nextConfig = {
         return [
             {
                 source: "/backend/:path*",
-                destination: `${apiTarget}/api/:path*`
+                destination: `${apiTarget}/:path*`
             }
         ];
     },
